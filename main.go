1724522e7b57236e97775e90301d36327603d519
Kang Seokhun <kangseokhun43@gmail.com>
2024-07-18 15:18:35 +0900

    add abi test code for merkle trie proof

--- main.go
+++ main.go
@@ -1,35 +1,101 @@
 package main
 
 import (
-	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 
-	"github.com/sircoon4/abi-test/vm/libplanet"
-	"github.com/sircoon4/bencodex-go"
-	"github.com/sircoon4/bencodex-go/bencodextype"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 )
 
 func main() {
-	serializedPayloadHackAndSlash := "ZDE6UzcwOjBEAiBREB4k6V+8VAXxIe9I1s565xeRyEhTcvft0QtkQavdRAIgF/7gAwEK1hCXBI3vStWHsInR56Pjbpgr77ZGNfYJWJ8xOmFsZHU3OnR5cGVfaWR1MTY6aGFja19hbmRfc2xhc2gyMnU2OnZhbHVlc2R1MTI6YXBTdG9uZUNvdW50dTE6MHUxMzphdmF0YXJBZGRyZXNzMjA63+Olmd3JqvwM5CMsow1BKkN5eE91ODpjb3N0dW1lc2xldTEwOmVxdWlwbWVudHNsMTY6k0hvMSQP3ESqxHe5FvP/ZDE2OnESdTcOEDhHjXX+a8k/kgMxNjputrpDman3Q5V424Y2EJutMTY6vGXnkjiTjk6uEDh/1UnKlTE2OoCmFqSy2FdGlMeyRVDTd0sxNjqAhR6pZsrMRoS+9euOX7TnMTY6yetsy/AeaEm0XJX2GX1KBWV1NTpmb29kc2xldTI6aWQxNjpVMrOqojpcTo+szFsaRu5tdTE6cmxsdTE6MHU1OjMwMDAxZWV1NzpzdGFnZUlkdTM6MjEwdTE0OnRvdGFsUGxheUNvdW50dTE6MXU3OndvcmxkSWR1MTo1ZWVlMTpnMzI6RYIlDQ2jOwZ3moR10oPV3SEMaDubmZ100D+sT1j6a84xOmxpNGUxOm1sZHUxMzpkZWNpbWFsUGxhY2VzMToSdTc6bWludGVyc251Njp0aWNrZXJ1NDpNZWFkZWkxMDAwMDAwMDAwMDAwMDAwMDAwZWUxOm5pMzExNmUxOnA2NToE3wbqFQmA1Y+s/9KKFfFC0scchb6fM0FbN2zIUcbvextqIZ+RGm9zXti+cBRBMhv+/ghXR6k08lAxOUH2iJvMYzE6czIwOmtPF4trn3OiAUK7/onUOjgBB4aDMTp0dTI3OjIwMjQtMDctMDVUMDk6MjM6MjAuNTE3Njc2WjE6dWxlZQ=="
-	data, err := base64.StdEncoding.DecodeString(serializedPayloadHackAndSlash)
+	proofAbi()
+	fmt.Println()
+	boolAbi()
+}
+
+func proofAbi() {
+	var stateRootHash []byte
+	var proof [][]byte
+	var key []byte
+	var value []byte
+
+	Bytes, _ := abi.NewType("bytes", "", nil)
+	BytesArr, _ := abi.NewType("bytes[]", "", nil)
+
+	var arguments = abi.Arguments{
+		abi.Argument{Name: "stateRootHash", Type: Bytes, Indexed: false},
+		abi.Argument{Name: "proof", Type: BytesArr, Indexed: false},
+		abi.Argument{Name: "key", Type: Bytes, Indexed: false},
+		abi.Argument{Name: "value", Type: Bytes, Indexed: false},
+	}
+
+	stateRootHash, proof, key, value = sampleFalseProof()
+
+	encoded, err := arguments.Pack(stateRootHash, proof, key, value)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	value, err := bencodex.Decode(data)
+	fmt.Println(hex.EncodeToString(encoded))
+
+	// decoded := map[string]any{
+	// 	"stateRootHash": nil,
+	// 	"proof":         nil,
+	// 	"key":           nil,
+	// 	"value":         nil,
+	// }
+	// err = arguments.UnpackIntoMap(decoded, encoded)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	// fmt.Println(decoded)
+}
+
+func boolAbi() {
+	var result bool
+
+	Bool, _ := abi.NewType("bool", "", nil)
+
+	var arguments = abi.Arguments{
+		abi.Argument{Name: "proofResult", Type: Bool, Indexed: false},
+	}
+
+	result = false
+
+	encoded, err := arguments.Pack(result)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	dict, ok := value.(*bencodextype.Dictionary)
-	if !ok {
-		fmt.Println("not a dictionary")
-		return
+	fmt.Println(hex.EncodeToString(encoded))
+}
+
+func simpleAbi() {
+	var a int64
+	var b int64
+
+	Int64, _ := abi.NewType("int64", "", nil)
+
+	var arguments = abi.Arguments{
+		abi.Argument{Name: "a", Type: Int64, Indexed: false},
+		abi.Argument{Name: "b", Type: Int64, Indexed: false},
 	}
-	abi, err := libplanet.ConvertToTransactionEthAbi(dict)
+
+	a = 1
+	b = 2
+
+	encoded, err := arguments.Pack(a, b)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(abi)
+	fmt.Println(encoded)
+
+	decoded, err := arguments.Unpack(encoded)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(decoded)
 }
